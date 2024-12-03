a7fb37217364083127658f1a9906e87cabb45f88
Haiyuan Lin <haiyuan.lin@tuni.fi>
2024-12-03 20:33:34 +0200

    files added

---
+++ ApolloServer/schema.js
@@ -0,0 +1,59 @@
+const typeDefs = `
+  type Author {
+    name: String
+    born: Int
+    bookCount: Int
+  }
+
+  type Book {
+    title: String!
+    published: Int!
+    author: Author!
+    genres: [String!]!
+    id: ID!
+  }
+
+  type User {
+    username: String!
+    genre: String!
+    id: ID!
+  }
+
+  type Token {
+    value: String!
+  }
+
+  type Query {
+    bookCount: Int
+    authorCount: Int
+    allBooks(author: String, genre: String): [Book]
+    allAuthors: [Author]
+    genres: [String!]!
+    me: User
+  }
+
+  type Mutation {
+    addBook(
+      title: String!
+      author: String!
+      published: Int!
+      genres: [String!]!
+    ): Book!
+
+    editAuthor(
+      name: String!
+      born: Int!
+    ): Author
+
+    createUser(
+      username: String!
+      favoriteGenre: String!
+    ): User
+
+    login(
+      username: String!
+      password: String!
+    ): Token
+}
+`
+module.exports = typeDefs
